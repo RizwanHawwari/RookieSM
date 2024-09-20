d0c052913a9fcd98ca66431ef6b4aa1aa01daaea
azzmsyriff <azzamsyarif01@gmail.com>
2024-09-20 20:40:09 +0700

    Menambahkan hero-image dan navbar

--- home.php
+++ home.php
@@ -1,14 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <link rel="stylesheet" href="css-file/home.css">
+  <title>Welcome to our website</title>
 </head>
-
 <body>
-  <a href="about">About</a>
-</body>
 
+<div class="navbar-links">
+  <ul>
+    <li>
+      <a href="#">Home</a>
+      <a href="#">About</a>
+      <a href="#">Products</a>
+    </li>
+  </ul>
+</div>
+
+<div class="hero-image">
+  <div class="hero-text">
+    <h1>M.A.N.T.A.P.</h1>
+    <p>SMK Citra Negara</p>
+  </div>
+</div>
+
+<footer></footer>
+</body>
 </html>
