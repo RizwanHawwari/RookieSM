978c1f7cf16660e20d488112f81b79274102cafb
Rizwan Hawwari <rzwnhawari@gmail.com>
2024-09-20 23:54:55 +0700

    Test Clean Url/Pretty Url

--- home.php
+++ home.php
@@ -1,30 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="css-file/home.css">
   <title>Welcome to our website</title>
 </head>
+
 <body>
 
-<div class="navbar-links">
-  <ul>
-    <li>
-      <a href="#">Home</a>
-      <a href="#">About</a>
-      <a href="#">Products</a>
-    </li>
-  </ul>
-</div>
-
-<div class="hero-image">
-  <div class="hero-text">
-    <h1>M.A.N.T.A.P.</h1>
-    <p>SMK Citra Negara</p>
+  <div class="navbar-links">
+    <ul>
+      <li>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Products</a>
+      </li>
+    </ul>
   </div>
-</div>
 
-<footer></footer>
+  <div class="hero-image">
+    <div class="hero-text">
+      <h1>M.A.N.T.A.P.</h1>
+      <p>SMK Citra Negara</p>
+    </div>
+  </div>
+
+  <footer></footer>
 </body>
+
 </html>
