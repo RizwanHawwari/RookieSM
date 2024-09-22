0465718637ae2d8fc2bbd071eb5f2d342a045810
arkaan <arkaansyafii@gmail.com>
2024-09-22 22:05:17 +0700

    Berhasil menghubungkan login form ke landing page

--- home.php
+++ home.php
@@ -1,14 +1,52 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="css-file/homie.css">
+  <title>Welcome to our website</title>
 </head>
-
 <body>
-  <a href="about">About</a>
-</body>
+  
+  <header class="header">
+    <div class="logo-container">
+      <img src="hero-img/cookie.svg" alt="Rookies Evolution" class="rookies">
+      <a href="https://smkscitranegara.sch.id" class="logo">Rookies School Management</a>
+    </div>
 
+    <nav class="navbar">
+      <a href="home.php">Home</a>
+      <a href="about.php">About</a>
+      <a href="product.php">Product</a>
+    </nav>
+  </header>
+
+  <section class="hero-section">
+    <div class="hero-text">
+      <h1>Welcome to Rookies School Management</h1>
+      <p>Empowering students through innovation and technology.</p>
+    </div>
+    <div class="hero-image">
+      <img src="hero-img/cookie.svg" alt="Rookies Evolution">
+    </div>
+  </section>
+
+  <footer>
+
+    <a href="login.php" class="login-button">Log In</a>
+
+    <div class="footerContainer">
+        <div class="socialIcons">
+            <a href=""><i class="fa-brands fa-github"></i>
+          <span class="social-title">GitHub</span></a>
+        </div>
+    </div>
+
+    <div class="footerBottom">
+        <p>Rookies School Management &copy2024</p>
+    </div>
+
+</footer>
+</body>
 </html>
