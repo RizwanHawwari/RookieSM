1c5d66bb8387134b5451f0708a583f1ee9bbe900
Rizwan Hawwari <rzwnhawari@gmail.com>
2024-09-22 22:05:24 +0700

    Fixing Some Display

--- home.php
+++ home.php
@@ -4,42 +4,21 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+    integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="css-file/homie.css">
   <title>Welcome to our website</title>
 </head>
 
 <body>
-  
+
   <header class="header">
     <div class="logo-container">
       <img src="hero-img/cookie.svg" alt="Rookies Evolution" class="rookies">
       <a href="https://smkscitranegara.sch.id" class="logo">Rookies School Management</a>
     </div>
 
-<<<<<<< HEAD
-  <div class="navbar-links">
-    <ul>
-      <li>
-        <a href="#">Home</a>
-        <a href="#">About</a>
-        <a href="#">Products</a>
-      </li>
-    </ul>
-  </div>
-
-  <div class="hero-image">
-    <div class="hero-text">
-      <h1>M.A.N.T.A.P.</h1>
-      <p>SMK Citra Negara</p>
-    </div>
-  </div>
-
-  <footer></footer>
-</body>
-
-</html>
-=======
     <nav class="navbar">
       <a href="home.php">Home</a>
       <a href="about.php">About</a>
@@ -62,17 +41,17 @@
     <a href="login.php" class="login-button">Log In</a>
 
     <div class="footerContainer">
-        <div class="socialIcons">
-            <a href=""><i class="fa-brands fa-github"></i>
+      <div class="socialIcons">
+        <a href=""><i class="fa-brands fa-github"></i>
           <span class="social-title">GitHub</span></a>
-        </div>
+      </div>
     </div>
 
     <div class="footerBottom">
-        <p>Rookies School Management &copy2024</p>
+      <p>Rookies School Management &copy2024</p>
     </div>
 
-</footer>
+  </footer>
 </body>
+
 </html>
->>>>>>> client-side
