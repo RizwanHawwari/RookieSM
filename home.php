9d04a25167fffc7b384b2aefda258a242209cbb4
Rizwan Hawwari <rzwnhawari@gmail.com>
2024-09-24 20:23:58 +0700

    Fix Navbar, Revisi Button Login & Register, Nambahin About Section

--- home.php
+++ home.php
@@ -7,42 +7,53 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
     integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
     crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <link rel="stylesheet" href="css-file/homie.css">
-  <title>Welcome to our website</title>
+  <link rel="stylesheet" href="home.css">
+  <title>Free Course | CN</title>
 </head>
 
 <body>
-
   <header class="header">
     <div class="logo-container">
       <img src="hero-img/cookie.svg" alt="Rookies Evolution" class="rookies">
-      <a href="https://smkscitranegara.sch.id" class="logo">Rookies School Management</a>
+      <a href="https://smkscitranegara.sch.id" class="logo">Free Course SMK Citra Negara</a>
     </div>
 
     <nav class="navbar">
       <a href="home.php">Home</a>
-      <a href="about.php">About</a>
+      <a href="#about">About</a>
       <a href="product.php">Product</a>
     </nav>
   </header>
 
   <section class="hero-section">
     <div class="hero-text">
-      <h1>Welcome to Rookies School Management</h1>
+      <h1>Welcome to Free Course SMK CITRA NEGARA</h1>
       <p>Empowering students through innovation and technology.</p>
+      <div class="nav-btn">
+        <a href="login.php" class="login-hero">Log In</a>
+        <a href="register.php" class="register-hero">Get Started</a>
+      </div>
     </div>
     <div class="hero-image">
       <img src="hero-img/cookie.svg" alt="Rookies Evolution">
     </div>
   </section>
 
-  <footer>
+  <section class="about">
+    <div id="about">
+      <h2>About Us</h2>
+      <p>Selamat datang di Free Course PPLG SMK Citra Negara! Kami adalah platform pembelajaran online gratis yang
+        dirancang untuk siswa jurusan Pengembangan Perangkat Lunak dan Gim (PPLG), dengan fokus pada pengembangan web.
+        Kursus yang kami tawarkan meliputi HTML & CSS, MySQL, PHP, dan JavaScript. Kami bertujuan untuk memberikan akses
+        materi belajar yang praktis dan relevan dengan kebutuhan industri teknologi.</p>
+    </div>
+  </section>
 
-    <a href="login.php" class="login-button">Log In</a>
+  <footer>
 
     <div class="footerContainer">
       <div class="socialIcons">
-        <a href=""><i class="fa-brands fa-github"></i>
+        <a href="https:/github.com/RizwanHawwari/RookieSM"><i class="fa-brands fa-github"></i>
           <span class="social-title">GitHub</span></a>
       </div>
     </div>
